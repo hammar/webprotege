46159c0bad912fc54933da25bd77dcced78e8128
Karl Hammar <karl@karlhammar.com>
2015-04-09 09:13:37 +0200

    Cleaned up specialization wizard GUI

--- src/main/java/edu/stanford/bmir/protege/web/client/xd/XdSpecializationWizard.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/xd/XdSpecializationWizard.java
@@ -1,12 +1,21 @@
 package edu.stanford.bmir.protege.web.client.xd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.OWLSubDataPropertyOfAxiom;
+import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLSubClassOfAxiomImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLSubDataPropertyOfAxiomImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLSubObjectPropertyOfAxiomImpl;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RadioButton;
-import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.gwtext.client.core.EventObject; 
 import com.gwtext.client.core.Position;
@@ -18,42 +27,45 @@ import com.gwtext.client.widgets.ToolbarButton;
 import com.gwtext.client.widgets.event.ButtonListenerAdapter;
 import com.gwtext.client.widgets.layout.CardLayout;
 
-public class XdSpecializationWizard extends Panel {
+public class XdSpecializationWizard extends com.gwtext.client.widgets.Window {
 
-	private Panel wizardPanel;
+	//private Panel wizardPanel;
 	private ToolbarButton backButton;
 	private ToolbarButton nextButton;
 	private ToolbarButton finishButton;
 	private Toolbar navigationBar;
 	private ButtonListenerAdapter navigationButtonsListener;
+	private CardLayout cardLayout;
+	
+	// Lists of user-selected subclass/subproperty axioms
+	private List<OWLSubClassOfAxiom> subClassAxioms;
+	private List<OWLSubDataPropertyOfAxiom> subDataPropertyAxioms;
+	private List<OWLSubObjectPropertyOfAxiom> subObjectPropertyAxioms;
+	
+	// Strategy selection radio buttons
+	RadioButton propStrategyButton;
+	RadioButton classStrategyButton;
+	RadioButton hybridStrategyButton;
 	
 	// Test field for class transfer
 	// TODO: remove this field
 	private OWLClass odpImplementation;
-	
-	// Dummy field for testing
-	// TODO: remove this field
-	private TextArea odpUriField;
-	
-	public ToolbarButton getFinishButton() {
-		return finishButton;
-	}
 
 	public XdSpecializationWizard() {
-		super();
-		this.setBorder(false);  
-        this.setPaddings(15); 
+		subClassAxioms = new ArrayList<OWLSubClassOfAxiom>();
+		subDataPropertyAxioms = new ArrayList<OWLSubDataPropertyOfAxiom>();
+		subObjectPropertyAxioms = new ArrayList<OWLSubObjectPropertyOfAxiom>();
 		
-        // General design stuff
-		wizardPanel = new Panel();
-		wizardPanel.setWidth(640);
-        wizardPanel.setHeight(480);  
-        wizardPanel.setTitle("ODP Specialisation Wizard");  
-        wizardPanel.setLayout(new CardLayout());  
-        wizardPanel.setActiveItem(0);  
-        wizardPanel.setPaddings(15);  
+		this.setTitle("ODP Specialisation Wizard");
+		this.setWidth(640);
+		this.setHeight(480);
+		this.setModal(true);
+		this.setResizable(false);
+		 
+		cardLayout = new CardLayout();
+		this.setLayout(cardLayout);
+		this.setActiveItem(0);
         
-  
         // This is the toolbar at the bottom of the wizard that handles back/forward navigation
         // through the wizard interface
         navigationBar = new Toolbar();  
@@ -68,99 +80,114 @@ public class XdSpecializationWizard extends Panel {
         nextButton.setId("move-next");  
         navigationBar.addButton(nextButton);
         
-        finishButton = new ToolbarButton("Finish");  
+        finishButton = new ToolbarButton("Finish", new ButtonListenerAdapter(){
+        	public void onClick(Button button, EventObject e) {
+        		saveAndClose();
+        	}
+        });  
         finishButton.setId("move-finish");  
         navigationBar.addButton(finishButton);  
-        wizardPanel.setBottomToolbar(navigationBar);  
+        this.setBottomToolbar(navigationBar);  
 
         
         // These are the individual cards/screens of the wizard interface.
-        wizardPanel.add(makeFirstCard());  
-        wizardPanel.add(makeSecondCard());  
-        wizardPanel.add(makeThirdCard()); 
-        
-        this.add(wizardPanel);
-        
-        XdServiceManager.getInstance().getOdpImplementation("http://doesnt.matter", new AsyncCallback<OWLClass>(){
+        this.add(makeFirstCard());  
+        this.add(makeSecondCard());  
+        this.add(makeThirdCard()); 
+	}
+	
+	private void saveAndClose() {
+		// TODO: implement this, wrap the below in an async service call that stores wizard results
+		this.hide();
+	}
+	
+	// This is where we clear out old data, load new data required 
+	// to run the wizard, prepare fields, etc etc
+	public void loadOdp(String uri) {
+		subClassAxioms.clear();
+		subDataPropertyAxioms.clear();
+		subObjectPropertyAxioms.clear();
+		
+		XdServiceManager.getInstance().getOdpImplementation(uri, new AsyncCallback<OWLClass>(){
 			@Override
 			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				Window.alert("Something went boom: " + caught.getMessage());
+				Window.alert("An error occured. ODP implementation could not be retrieved from GWT back-end. Error message: " + caught.getMessage());
 			}
 
 			@Override
 			public void onSuccess(OWLClass result) {
-				// TODO Auto-generated method stub
 				odpImplementation = result;
-				Window.alert("Fetched ODP " + odpImplementation.getIRI().toString());
-				odpUriField.setText(odpImplementation.getIRI().toString());
 			}
         });
-	}
-	
-	
-	// This is where we do things that reset the wizard GUI, preparing it to start over again.
-	public void reset() {
-		// TODO: Implement this
 	}
 	
 	
 	private ButtonListenerAdapter makeNavigationButtonsListenerAdapter() {
 		return new ButtonListenerAdapter() {  
             public void onClick(Button button, EventObject e) {  
-                String btnID = button.getId();  
-                CardLayout cardLayout = (CardLayout) wizardPanel.getLayout();  
+                String btnID = button.getId();    
                 String panelID = cardLayout.getActiveItem().getId();  
   
-                if (btnID.equals("move-prev")) {  
-                    if (panelID.equals("card-3")) {  
-                        cardLayout.setActiveItem(1);  
-                    } else {  
-                        cardLayout.setActiveItem(0);  
+                if (btnID.equals("move-prev")) {
+                    if (panelID.equals("card-2")) {
+                    	// From last to middle card
+                        cardLayout.setActiveItem(1);
+                    }
+                    else {
+                    	// From middle to first card
+                        cardLayout.setActiveItem(0);
                     }  
-                } else {  
-  
-                    if (panelID.equals("card-1")) {  
-                        cardLayout.setActiveItem(1);  
-                    } else {  
-                        cardLayout.setActiveItem(2);  
+                } 
+                else {  
+                    if (panelID.equals("card-0")) {
+                    	// From first to second card
+                        cardLayout.setActiveItem(1);
+                        // TODO: Clear out old suggestions from UI
+                        // TODO: Call service back-end to get suggested specialization axioms
+                        // TODO: Populate UI with suggestions
+                    }
+                    else {
+                    	// From second to third card
+                        cardLayout.setActiveItem(2);
+                        // TODO: Clear out old rendering from UI
+                        // TODO: Render specialization graphically
                     }  
-                }  
-            }  
+                }
+            }
         };  
 	}
 	
 	private Panel makeFirstCard() {
         Panel first = new Panel();  
         first.setBorder(false);
-        first.setId("card-1");
+        first.setId("card-0");
         first.setTitle("Specialisation Strategy");
         
-        RadioButton strategyButton1 = new RadioButton("strategyGroup", "Property-oriented strategy");
-        RadioButton strategyButton2 = new RadioButton("strategyGroup", "Class-oriented strategy");
-        RadioButton strategyButton3 = new RadioButton("strategyGroup", "Hybrid strategy");
+        propStrategyButton = new RadioButton("strategyGroup", "Property-oriented strategy");
+        classStrategyButton = new RadioButton("strategyGroup", "Class-oriented strategy");
+        hybridStrategyButton = new RadioButton("strategyGroup", "Hybrid strategy");
 
        // Add toggle button to the root panel.
        VerticalPanel strategyPanel = new VerticalPanel();
        strategyPanel.setSpacing(10);
        strategyPanel.add(new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rhoncus sapien. Phasellus vitae magna at enim molestie auctor et at est. Nullam consequat odio eu varius venenatis. Etiam aliquet ligula ac posuere lacinia. Curabitur aliquet elit a viverra fringilla. "));
-       strategyPanel.add(strategyButton1);
+       strategyPanel.add(propStrategyButton);
        strategyPanel.add(new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rhoncus sapien. Phasellus vitae magna at enim molestie auctor et at est. Nullam consequat odio eu varius venenatis. Etiam aliquet ligula ac posuere lacinia. Curabitur aliquet elit a viverra fringilla. "));
-       strategyPanel.add(strategyButton2);
+       strategyPanel.add(classStrategyButton);
        strategyPanel.add(new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rhoncus sapien. Phasellus vitae magna at enim molestie auctor et at est. Nullam consequat odio eu varius venenatis. Etiam aliquet ligula ac posuere lacinia. Curabitur aliquet elit a viverra fringilla. "));
-       strategyPanel.add(strategyButton3);
+       strategyPanel.add(hybridStrategyButton);
        first.add(strategyPanel);
        return first;
 	}
 	
 	private Panel makeSecondCard() {
-        Panel second = new Panel();  
+        Panel second = new Panel();
         second.setBorder(false);  
-        second.setId("card-2");
+        second.setId("card-1");
         second.setTitle("Specialise Classes and Properties");
         second.add(new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in rhoncus sapien. Phasellus vitae magna at enim molestie auctor et at est. Nullam consequat odio eu varius venenatis. Etiam aliquet ligula ac posuere lacinia. Curabitur aliquet elit a viverra fringilla. "));
         
-        TabPanel tabPanel = new TabPanel();  
+        TabPanel tabPanel = new TabPanel();
         tabPanel.setTabPosition(Position.TOP);  
         tabPanel.setResizeTabs(true);  
         tabPanel.setMinTabWidth(115);
@@ -193,7 +220,7 @@ public class XdSpecializationWizard extends Panel {
 	private Panel makeThirdCard() {
         Panel third = new Panel();  
         third.setBorder(false);  
-        third.setId("card-3");
+        third.setId("card-2");
         third.setTitle("Specialisation Overview");
         third.setHtml("<h1>Congratulations!</h1><p>Step 3 of 3 - Complete</p>");  
         return third;
