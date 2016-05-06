b83af5a6a195ce332a622fd7f16fd6562242567b
Karl Hammar <karl@karlhammar.com>
2016-05-06 16:01:25 +0200

    Got wizard infrastructure working

--- src/main/java/edu/stanford/bmir/protege/web/client/xd/specialization/DesignPatternInstantiationWizard.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/xd/specialization/DesignPatternInstantiationWizard.java
@@ -1,6 +1,8 @@
 package edu.stanford.bmir.protege.web.client.xd.specialization;
 
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -10,6 +12,7 @@ import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
 
 import edu.stanford.bmir.protege.web.client.xd.DesignPatternDetailsPortlet;
+import edu.stanford.bmir.protege.web.client.xd.specialization.panels.AlignmentsPanel;
 import edu.stanford.bmir.protege.web.client.xd.specialization.panels.InstantiationMethodSelectionPanel;
 import edu.stanford.bmir.protege.web.client.xd.specialization.panels.PreviewPanel;
 import edu.stanford.bmir.protege.web.shared.xd.data.CodpInstantiationMethod;
@@ -24,6 +27,7 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 	private Panel wizardPanel;
 	private Panel visualizationPanel;
 	private InstantiationMethodSelectionPanel instantiationMethodSelectionPanel;
+	private AlignmentsPanel alignmentsPanel;
 	private PreviewPanel previewPanel;
 	private CodpInstantiationMethod instantiationMethod;
 	private ActiveWizardScreen activeWizardScreen;
@@ -34,20 +38,32 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 	
 	public DesignPatternInstantiationWizard(DesignPatternDetailsPortlet parent) {
 		super(false, true);
-		
-		// TODO: figure out how to display title in UI
-		this.setTitle("CODP Instantiation Wizard");
 		this.addStyleName("xdInstantiationWizard");
+		
+		DockLayoutPanel wizardFramePanel = new DockLayoutPanel(Unit.EM);
+		// Size of popup widget (i.e., framing panel) governs size of popup, so is set here.
+		wizardFramePanel.setHeight("480px");
+		wizardFramePanel.setWidth("640px");
+		
+		// Header holding wizard title and close button
+		DockLayoutPanel wizardHeaderPanel = new DockLayoutPanel(Unit.EM);
+		wizardHeaderPanel.addStyleName("xdWizardHeader");
+		Button closeWizardButton = new Button("X");
+		closeWizardButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent arg0) {
+				hide();
+			}
+		});
+		wizardHeaderPanel.addEast(closeWizardButton, 2);
+		wizardHeaderPanel.add(new Label("CODP Instantiation Wizard"));
+		wizardFramePanel.addNorth(wizardHeaderPanel, 2);
 		
 		// Default values
 		this.instantiationMethod = CodpInstantiationMethod.TEMPLATE_BASED;
 		
 		// Outer tab panel containing ODP visualization and instantiation wizard
 		TabLayoutPanel tabPanel = new TabLayoutPanel(3, Unit.EM);
-		
-		// Size of popup widget (i.e., tab panel) governs size of popup, so is set here.
-		tabPanel.setHeight("480px");
-		tabPanel.setWidth("640px");
 		
 		// Add tabs
 		this.wizardPanel = makeWizardPanel();
@@ -56,8 +72,11 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 		tabPanel.add(visualizationPanel, "CODP Visualisation");
 		tabPanel.selectTab(wizardPanel);
 		
+		// Add tab panel to main body of outer frame panel
+		wizardFramePanel.add(tabPanel);
+		
 		// Set popup widget to be the tab panel
-		this.setWidget(tabPanel);
+		this.setWidget(wizardFramePanel);
 	}
 
 	private DockLayoutPanel makeWizardPanel() {
@@ -67,38 +86,23 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 		DockLayoutPanel navBar = new DockLayoutPanel(Unit.EM);
 		navBar.addStyleName("xdWizardNavBar");
 		this.wizardBackButton = new Button("Back");
+		this.wizardBackButton.addClickHandler(makeBackButtonClickHandler());
 		this.wizardNextButton = new Button("Next");
+		this.wizardNextButton.addClickHandler(makeNextButtonClickHandler());
 		this.wizardFinishButton = new Button("Finish");
-		// TODO: implement button listeners that moves user between screens
+		this.wizardFinishButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent arg0) {
+				saveAndClose();
+			}
+		});
 		navBar.addWest(this.wizardBackButton, 5);
 		navBar.addEast(this.wizardNextButton, 5);
 		navBar.addEast(this.wizardFinishButton, 5);
-		// Hide finish button (will be shown when user goes to last screen of wizard)
+		// Set initial button state
+		this.wizardBackButton.setEnabled(false);
 		this.wizardFinishButton.setVisible(false);
 		wp.addSouth(navBar, 3);
-		
-		/*
-		Toolbar navigationBar = new Toolbar();  
-        
-        this.wizardBackButton = new ToolbarButton("Back", makeBackButtonListener());  
-        this.wizardBackButton.setId("move-prev");  
-        navigationBar.addButton(wizardBackButton);  
-        navigationBar.addFill();  
-  
-        this.wizardNextButton = new ToolbarButton("Next", makeForwardButtonListener());  
-        this.wizardNextButton.setId("move-next");  
-        navigationBar.addButton(wizardNextButton);
-        
-        this.wizardFinishButton = new ToolbarButton("Finish", new ButtonListenerAdapter(){
-        	public void onClick(Button button, EventObject e) {
-        		saveAndClose();
-        	}
-        });
-        wizardFinishButton.setId("move-finish");  
-        navigationBar.addButton(wizardFinishButton);
-        wizardFinishButton.setVisible(false);
-        wp.setBottomToolbar(navigationBar);
-        */
         
 		// This is the main content field of the wizard interface
 		FlowPanel wpContentPanel = new FlowPanel(); 
@@ -106,13 +110,15 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 		
         // These are the individual cards/screens of the wizard interface.
         this.instantiationMethodSelectionPanel = new InstantiationMethodSelectionPanel(this);
+        this.alignmentsPanel = new AlignmentsPanel(this);
         this.previewPanel = new PreviewPanel(this);
         wpContentPanel.add(this.instantiationMethodSelectionPanel);
-        //wpContentPanel.add(this.previewPanel);
+        wpContentPanel.add(this.alignmentsPanel);
+        wpContentPanel.add(this.previewPanel);
         
-        // Hide all but the first subpanel of the wizard
+        // Set initial wizard state
+        this.alignmentsPanel.setVisible(false);
         this.previewPanel.setVisible(false);
-        // Set flag of which screen is active to default first value
         this.activeWizardScreen = ActiveWizardScreen.METHOD_SELECTION;
         
         wp.add(wpContentPanel);
@@ -121,7 +127,6 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 	
 	private Panel makeVisualizationPanel() {
 		FlowPanel p = new FlowPanel();
-		//p.setTitle("CODP Visualisation");
 		p.add(new Label("Visualisation goes here"));
 		return p;
 	}
@@ -130,63 +135,82 @@ public class DesignPatternInstantiationWizard extends PopupPanel {
 		// TODO Auto-generated method stub
 	}
 	
-	/*
-	private ButtonListenerAdapter makeBackButtonListener() {
-		return new ButtonListenerAdapter() {  
-            public void onClick(Button button, EventObject e) {      
-                String panelID = wizardCardLayout.getActiveItem().getId();  
-  
-                switch (panelID) {
-                case "card-1":
-                	wizardCardLayout.setActiveItem(0);
-                	break;
-                	
-                case "card-2":
-                	wizardCardLayout.setActiveItem(1);
-                	break;
-                	
-                case "card-3":
-                	wizardCardLayout.setActiveItem(2);
-                	break;
-                	
-                case "card-4":
-                	wizardCardLayout.setActiveItem(3);
-                	wizardFinishButton.setVisible(false);
-                    wizardNextButton.setVisible(true);
-                	break;
-                } 
-            }
-        };
+	private ClickHandler makeBackButtonClickHandler() {
+		return new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent arg0) {
+				switch (activeWizardScreen) {
+				case METHOD_SELECTION:
+					// This should not be able to occur
+					break;
+					
+				case ALIGNMENTS:
+					//Moving ALIGNMENTS->METHOD_SELECTION
+					// Set panel visibility state
+					alignmentsPanel.setVisible(false);
+					instantiationMethodSelectionPanel.setVisible(true);
+					// Set button state
+					wizardBackButton.setEnabled(false);
+					// Set statekeeping enum
+					activeWizardScreen = ActiveWizardScreen.METHOD_SELECTION;
+					// TODO: Re-render newly selected panel
+					break;
+				
+				case PREVIEW:
+					// Moving PREVIEW->ALIGNMENTS
+					// Set panel visibility state
+					previewPanel.setVisible(false);
+					alignmentsPanel.setVisible(true);
+					// Set button state
+					wizardFinishButton.setVisible(false);
+					wizardNextButton.setVisible(true);
+					// Set statekeeping enum
+					activeWizardScreen = ActiveWizardScreen.ALIGNMENTS;
+					// TODO: Re-render newly selected panel
+					break;
+				}
+			}
+		};
 	}
-
-	private ButtonListenerAdapter makeForwardButtonListener() {
-		return new ButtonListenerAdapter() {  
-            public void onClick(Button button, EventObject e) {  
-                String panelID = wizardCardLayout.getActiveItem().getId();  
-  
-                switch (panelID) {
-                case "card-0":
-                	wizardCardLayout.setActiveItem(1);
-                	break;
-                	
-                case "card-1":
-                	wizardCardLayout.setActiveItem(2);
-                	break;
-                	
-                case "card-2":
-                	wizardCardLayout.setActiveItem(3);
-                	break;
-                	
-                case "card-3": 
-                	wizardCardLayout.setActiveItem(4);
-                	wizardFinishButton.setVisible(true);
-                    wizardNextButton.setVisible(false);
-                	break;
-                }
-            }
-        };
+	
+	private ClickHandler makeNextButtonClickHandler() {
+		return new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent arg0) {
+				// TODO: fix this when more screens are added
+				switch (activeWizardScreen) {
+				case METHOD_SELECTION:
+					// Moving METHOD_SELECTION -> ALIGNMENTS
+					// Set panel visibility state
+					instantiationMethodSelectionPanel.setVisible(false);
+					alignmentsPanel.setVisible(true);
+					// Set button state
+					wizardBackButton.setEnabled(true);
+					// Set statekeeping enum
+					activeWizardScreen = ActiveWizardScreen.ALIGNMENTS;
+					// TODO: Re-render newly selected panel
+					break;
+					
+				case ALIGNMENTS:
+					// Moving ALIGNMENTS->PREVIEW
+					// Set panel visibility state
+					alignmentsPanel.setVisible(false);
+					previewPanel.setVisible(true);
+					// Set button state
+					wizardNextButton.setVisible(false);
+					wizardFinishButton.setVisible(true);
+					// Set statekeeping enum
+					activeWizardScreen = ActiveWizardScreen.PREVIEW;
+					// TODO: Re-render newly selected panel
+					break;
+					
+				case PREVIEW:
+					// This should not be able to occur
+					break;
+				}
+			}
+		};
 	}
-	*/
 	
 	public OdpSpecialization getSpecialization() {
 		// TODO Auto-generated method stub
--- src/main/java/edu/stanford/bmir/protege/web/client/xd/specialization/panels/AlignmentsPanel.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/xd/specialization/panels/AlignmentsPanel.java
@@ -1,35 +1,30 @@
 package edu.stanford.bmir.protege.web.client.xd.specialization.panels;
 
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.gwtext.client.widgets.Panel;
-import com.gwtext.client.widgets.layout.RowLayout;
-import com.gwtext.client.widgets.layout.RowLayoutData;
 
 import edu.stanford.bmir.protege.web.client.xd.specialization.DesignPatternInstantiationWizard;
 
-public class AlignmentsPanel extends Panel {
+public class AlignmentsPanel extends DockLayoutPanel {
 	
 	private DesignPatternInstantiationWizard parentWizard;
 	
 	public AlignmentsPanel(DesignPatternInstantiationWizard parent) {
-		super();
+		super(Unit.EM);
 		
 		this.parentWizard = parent;
 		
-        this.setBorder(false);  
-        this.setId("card-3");
         this.setTitle("CODP Specialisation Alignment");
-        this.setLayout(new RowLayout());
         
-        Panel instructionPanel = new Panel();
-        instructionPanel.setPaddings(5);
+        FlowPanel instructionPanel = new FlowPanel();
         Label instruction = new Label("From the suggested entity alignments listed below, constructed based on your specialised "
         		+ "entities and the existing ontology entities, please select the ones that hold within your model. Please note "
         		+ "that for multiple simultanous selection to work you must check the boxes individually, not just click on "
         		+ "the rows in question. ");
         instructionPanel.add(instruction);
-        this.add(instructionPanel, new RowLayoutData(53));
-        
+        this.addNorth(instructionPanel, 4);
         
         this.add(new Label("Alignment interface to be implemented."));
 	}
