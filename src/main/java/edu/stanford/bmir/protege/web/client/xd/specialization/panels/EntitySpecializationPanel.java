7890ee7660627a1de0cde7cae0900d84fe5f7d05
Karl Hammar <karl@karlhammar.com>
2016-06-07 17:17:58 +0200

    Further work on entity specialisation UI

--- src/main/java/edu/stanford/bmir/protege/web/client/xd/specialization/panels/EntitySpecializationPanel.java
+++ src/main/java/edu/stanford/bmir/protege/web/client/xd/specialization/panels/EntitySpecializationPanel.java
@@ -1,18 +1,16 @@
 package edu.stanford.bmir.protege.web.client.xd.specialization.panels;
 
-import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.CheckBox;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import edu.stanford.bmir.protege.web.client.xd.specialization.DesignPatternInstantiationWizard;
+import edu.stanford.bmir.protege.web.client.xd.specialization.widgets.EntityTreeNode;
 import edu.stanford.bmir.protege.web.shared.xd.data.FrameTreeNode;
 import edu.stanford.bmir.protege.web.shared.xd.data.entityframes.OntologyEntityFrame;
 
@@ -28,12 +26,12 @@ public class EntitySpecializationPanel extends VerticalPanel implements Instanti
 		this.setSpacing(10);
 		
 		this.setTitle("Entity Specialization");
-		
         Label instruction = new Label("Please select the classes, object properties, and datatype properties from "
         		+ "the ontology design pattern that you wish to specialize for your modeling need from the list "
         		+ "below.");
 		this.add(instruction);
 		
+		// Toolbar by which we interact with tree
 		HorizontalPanel editingToolbar = new HorizontalPanel();
 		editingToolbar.addStyleName("entitySpecializationToolbar");
 		HorizontalPanel innerEditingToolbar = new HorizontalPanel();
@@ -47,18 +45,26 @@ public class EntitySpecializationPanel extends VerticalPanel implements Instanti
 		editingToolbar.add(innerEditingToolbar);
 		this.add(editingToolbar);
 		
-		// TODO: implement real data loading here
+		// The tree itself
 		entityTree = new Tree();
 		entityTree.addStyleName("entityTree");
+		entityTree.addSelectionHandler(new SelectionHandler<TreeItem>() {
+			@Override
+			public void onSelection(SelectionEvent<TreeItem> event) {
+				OntologyEntityFrame oef = (OntologyEntityFrame)event.getSelectedItem().getUserObject();
+				if (oef.getIri().isPresent()) {
+					modifyButton.setEnabled(false);
+					deleteButton.setEnabled(false);
+	        	}
+	        	else {
+	        		modifyButton.setEnabled(true);
+					deleteButton.setEnabled(true);
+				}
+			}
+		});
 		TreeItem root = new TreeItem();
-	    root.setText("root");
-	    root.addTextItem("item0");
-	    root.addTextItem("item1");
-	    root.addTextItem("item2");
-	    TreeItem item = new TreeItem(new CheckBox("item3"));
-	    root.addItem(item);
+	    root.setText("Placeholder");
 	    entityTree.addItem(root);
-	    root.setState(true);
 		this.add(entityTree);
 	}
 
@@ -66,31 +72,32 @@ public class EntitySpecializationPanel extends VerticalPanel implements Instanti
 
 	@Override
 	public void renderPanel() {
-		// TODO: Build using cell tree instead?
-		
 		// Clear out existing tree
 		entityTree.clear();
 		
-		// Render classes
-		FrameTreeNode<OntologyEntityFrame> classTree = parentWizard.getOdpClasses();
-		TreeItem rootClass = new TreeItem();
-		for (FrameTreeNode<OntologyEntityFrame> subClass: classTree.getChildren()) {
-			recursivelyAddClassNode(subClass, rootClass);
-		}
-		entityTree.addItem(rootClass);
-		rootClass.setState(true);
-		
-		// TODO: Render object properties and datatype properties
+		// Render classes, object properties, and datatype properties
+		renderEntityTree(parentWizard.getOdpClasses());
+		renderEntityTree(parentWizard.getOdpObjectProperties());
+		renderEntityTree(parentWizard.getOdpDataProperties());
 	}
 	
-	private void recursivelyAddClassNode(FrameTreeNode<OntologyEntityFrame> childNodeFromServer, TreeItem parentNode) {
-		OntologyEntityFrame childClassFrame = childNodeFromServer.getData();
-		TreeItem newChildNode = new TreeItem(new Label(childClassFrame.getLabel()));
-		newChildNode.addStyleName("owlClassTreeNode");
-		newChildNode.setUserObject(childClassFrame);
+	private void renderEntityTree(FrameTreeNode<OntologyEntityFrame> inputFrameTree) {
+		TreeItem rootItem = new TreeItem(new EntityTreeNode(inputFrameTree.getData()));
+		for (FrameTreeNode<OntologyEntityFrame> childFrame: inputFrameTree.getChildren()) {
+			recursivelyAddEntityNode(childFrame, rootItem);
+		}
+		entityTree.addItem(rootItem);
+		rootItem.setState(true);
+	}
+	
+	private void recursivelyAddEntityNode(FrameTreeNode<OntologyEntityFrame> childNodeFromServer, TreeItem parentNode) {
+		OntologyEntityFrame childFrame = childNodeFromServer.getData();
+		TreeItem newChildNode = new TreeItem(new EntityTreeNode(childFrame));
+		newChildNode.setUserObject(childFrame);
 		parentNode.addItem(newChildNode);
+		parentNode.setState(true);
 		for (FrameTreeNode<OntologyEntityFrame> nextChild: childNodeFromServer.getChildren()) {
-			recursivelyAddClassNode(nextChild, newChildNode);
+			recursivelyAddEntityNode(nextChild, newChildNode);
 		}
 	}
 	
